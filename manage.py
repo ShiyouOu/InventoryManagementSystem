03cc3a8bab1a0807e39f835ab9e90af5d3adc2a6
Ubuntu <ubuntu@ip-172-31-5-15.us-west-1.compute.internal>
2022-04-16 02:45:16 +0000

    Updates to CSS and added form for adding products

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def hello_world():
 	return	render_template("index.html")
 
 
-@app.route('/summary')
+@app.route('/summary/')
 def summary():
     con = sql.connect("database.db")
     con.row_factory = sql.Row
@@ -20,9 +20,25 @@ def summary():
     rows = cur.fetchall()
     return render_template("summary.html", rows = rows)
 
-@app.route('/add')
+@app.route('/add/')
 def add():
 	return	render_template("add_inventory.html")
+
+@app.route('/addproduct/', methods = ["POST", "GET"])
+def add_product():
+    if request.method == "POST":
+        data = request.form
+        nm = data['nm']
+        desc = data['desc']
+        quantity = data['quantity']
+        checkin = data['checkin']
+
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO products (name, description, quantity, checkin) VALUES ('{0}', '{1}', '{2}', '{3}')".format(nm, desc, quantity, checkin))
+            con.commit()
+        return redirect(url_for('summary'))
+    return "Error"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
